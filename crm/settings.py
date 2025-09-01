870f833d70e1bb649c9019dd8f914f58c7cfc39a
george-ibuchi <georgeibuchi44@gmail.com>
2025-09-01 04:57:43 +0100

    Configure Celery With Beat scheduler for weekly CRM report

--- crm/settings.py
+++ crm/settings.py
@@ -16,3 +16,7 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(day_of_week='mon', hour=6, minute=0),
     },
 }
+
+# Celery Broker & Backend (Redis)
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
